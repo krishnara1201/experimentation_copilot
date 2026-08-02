8d3a3d970e75de0fbe612ed4218e311684a853c2
krishnara1201 <krish.narayanan1201@gmail.com>
2026-08-01 21:09:56 -0400

    adding more calcs and models

--- backend/app/db/models/metric_model.py
+++ backend/app/db/models/metric_model.py
@@ -5,11 +5,8 @@ from enum import Enum
 from app.db.models.experiment_model import Experiment
 
 class Metric_type(str, Enum):
-    NUMERIC = "numeric"
-    CATEGORICAL = "categorical"
-    BOOLEAN = "boolean"
+    BINARY = "binary"
     CONTINUOUS = "continuous"
-    ORDINAL = "ordinal"
 
 class Metric_direction(str, Enum):
     UP = "up"
@@ -21,7 +18,7 @@ class Metric(SQLModel, table=True):
     name: str = Field(index=True)
     experiment_id: int = Field(foreign_key="experiment.id")
     direction: Metric_direction = Field(default=Metric_direction.UP, index=True)
-    type: Metric_type = Field(default=Metric_type.NUMERIC, index=True)
+    type: Metric_type = Field(default=Metric_type.binary, index=True)
     is_primary: bool = Field(default=False)
     is_guardrail: bool = Field(default=False)
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc).replace(tzinfo=None))
